a00fed7e8a4e6a7f65977f9c67deb30674457ed5
krateng <git.noreply@krateng.ch>
2021-04-24 19:42:20 +0200

    Removed Greeting

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,28 +10,7 @@ function date() {
   document.getElementById("header_date").innerHTML = date;
 }
 
-function greet() {
-  let currentTime = new Date();
-  let greet = Math.floor(currentTime.getHours() / 6);
-  switch (greet) {
-    case 0:
-      document.getElementById("header_greet").innerHTML = "Good night!";
-      break;
-    case 1:
-      document.getElementById("header_greet").innerHTML = "Good morning!";
-      break;
-    case 2:
-      document.getElementById("header_greet").innerHTML = "Good afternoon!";
-      break;
-    case 3:
-      document.getElementById("header_greet").innerHTML = "Good evening!";
-      break;
-  }
-}
 
 function loadFunctions() {
-  date();  
-  greet();
+  date();
 }
-
-
